635b434af1afa9cbca08847de708e35e7816f650
Ukashashere <116743795+Ukashashere@users.noreply.github.com>
2024-04-18 05:45:15 +0530

    Additional features

--- src/mongodb.js
+++ src/mongodb.js
@@ -19,9 +19,9 @@ const logInSchema=new mongoose.Schema({        //To create a database / a docume
     }
 })
 
-const LogInCollection=new mongoose.model('LogInCollection',logInSchema)  //Collection part, we are defining the collection here. 'LogInCollection' is name of collection. 'logInSchema' is written coz, it'll follow it's schema.
+const collection=new mongoose.model('LogInCollection',logInSchema)  //Collection part, we are defining the collection here. 'LogInCollection' is name of collection. 'logInSchema' is written coz, it'll follow it's schema.
 
-module.exports=LogInCollection
+module.exports=collection
 
 
 
